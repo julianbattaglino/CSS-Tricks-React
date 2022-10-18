f03c6f5f812df33a8034eec7ed35ae2c1f14f242
Julian Battaglino <julianbattaglino@gmail.com>
2022-10-18 20:36:47 -0300

    add code blocks

--- src/components/Underline/Underline.js
+++ src/components/Underline/Underline.js
@@ -1,12 +1,35 @@
 import "./Underline.css";
-import "../../assets/css/stackoverflow-dark.css";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Underline = () => {
+
+    const thinUnderlineHtml = `
+<!-- HTML -->
+<h2><span className="fancy-underline-blue underline-thin">Thin Underline</span></h2>
+`
+    const thinUnderlineCss = `
+/* CSS */
+.fancy-underline-blue {
+    background-image: linear-gradient(120deg, rgba(93, 178, 254, 0.4) 0%, rgba(93, 178, 254, 0.4) 100%);
+    background-repeat: no-repeat;
+    background-position: 0 100%;
+    transition: background-size 0.25s ease-in;
+    width: fit-content;
+}
+`
     return (
         <>
             <div className="grid-container background-grey pt-80">
                 <div className="col">
                     <h2><span className="fancy-underline-blue underline-thin">Thin Underline</span></h2>
+                    <SyntaxHighlighter language="html" style={vscDarkPlus}>
+                        {thinUnderlineHtml}
+                    </SyntaxHighlighter>
+
+                    <SyntaxHighlighter language="css" style={vscDarkPlus}>
+                        {thinUnderlineCss}
+                    </SyntaxHighlighter>
                 </div>
 
                 <div className="col">
@@ -30,7 +53,7 @@ const Underline = () => {
 
                 <div className="col">
                     <div className="flex-container">
-                        <a href="/" className="link link--carme">
+                        <a href="/" className="link link--drawned">
                             <span>Drawned</span>
                             <svg className="link__graphic link__graphic--stroke link__graphic--scribble" width="100%" height="9" viewBox="0 0 101 9"><path d="M.426 1.973C4.144 1.567 17.77-.514 21.443 1.48 24.296 3.026 24.844 4.627 27.5 7c3.075 2.748 6.642-4.141 10.066-4.688 7.517-1.2 13.237 5.425 17.59 2.745C58.5 3 60.464-1.786 66 2c1.996 1.365 3.174 3.737 5.286 4.41 5.423 1.727 25.34-7.981 29.14-1.294" pathLength="1" /></svg>
                         </a>
