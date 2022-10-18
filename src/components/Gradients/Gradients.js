c580dda30b158fb0d7cdd097093002b73dbd5382
Julian Battaglino <julianbattaglino@gmail.com>
2022-10-18 20:36:57 -0300

    codepen iframe

--- src/components/Gradients/Gradients.js
+++ src/components/Gradients/Gradients.js
@@ -10,7 +10,11 @@ const Gradients = () => {
 
             <div className="col">
                 <h2><span class="text-gradient-2">Text Gradient.</span></h2>
-            </div>
+                <iframe scrolling="no" title="Text Gradient" src="https://codepen.io/julianbattaglino/embed/MWGLzWR?default-tab=html&theme-id=dark" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+                    See the Pen <a href="https://codepen.io/julianbattaglino/pen/MWGLzWR">
+                        Text Gradient</a> by julianbattaglino (<a href="https://codepen.io/julianbattaglino">@julianbattaglino</a>)
+                    on <a href="https://codepen.io">CodePen</a>.
+                </iframe>            </div>
 
             <div className="col">
                 <h2 class="gradient-text">Text Gradient</h2>
