3bba9d736456f0faf632e0fc259a8a3b99a0abea
Julian Battaglino <julianbattaglino@gmail.com>
2022-10-18 15:27:00 -0300

    404 page add

---
+++ src/components/Page404/Page404.js
@@ -0,0 +1,10 @@
+const Page404 = () => {
+
+    return (
+        <div className="title-container">
+            <h1 className="main-title">404 Error Page not found</h1>
+        </div>
+    )
+}
+
+export default Page404;
